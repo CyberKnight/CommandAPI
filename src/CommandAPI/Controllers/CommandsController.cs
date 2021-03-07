3407c7af80591347ec088f6b4085bd20f35fef83
Lee Achorn <cyberknight65@gmail.com>
2021-03-07 10:05:34 -0500

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,30 +2,52 @@ using System;
 using System.Globalization;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase{
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] {"this", "is", "hard", "coded"};
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var Commands = _repository.GetAllCommands();
+            return Ok(Commands);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var Command = _repository.GetCommandById(id);
+            if (Command == null)
+            {
+                return NotFound();
+            }
+            return Ok(Command);
         }
     }
 
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HealthController : ControllerBase{
-        [HttpGet]
-        public ActionResult<string> Get()
-        {
-            DateTime localDate = DateTime.Now;
-            String cultureName = "en-US";
-            var culture = new CultureInfo(cultureName);
-            String rc = string.Concat("The time is now ", localDate.ToString(culture));
-            return(rc);
-        }
-    }
+    // [Route("api/[controller]")]
+    // [ApiController]
+    // public class HealthController : ControllerBase{
+    //     [HttpGet]
+    //     public ActionResult<string> Get()
+    //     {
+    //         DateTime localDate = DateTime.Now;
+    //         String cultureName = "en-US";
+    //         var culture = new CultureInfo(cultureName);
+    //         String rc = string.Concat("The time is now ", localDate.ToString(culture));
+    //         return(rc);
+    //     }
+    // }
+
 }
